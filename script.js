c89ab852def55225f3d0a43891a4d7527ba0d010
Jared <68973848+JRH89@users.noreply.github.com>
2023-01-09 12:42:16 -0800

    Initial Commit!
    v2.0 of CovidCounter

---
+++ script.js
@@ -0,0 +1,22 @@
+// Fetch the latest COVID-19 statistics from a public API
+const apiUrl = 'https://api.covid19api.com/summary';
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Update the total case and death counts
+    document.getElementById('total-cases').innerHTML = data.Global.TotalConfirmed;
+    document.getElementById('total-deaths').innerHTML = data.Global.TotalDeaths;
+    // Add a row to the cases by country table for each country
+    data.Countries.forEach(country => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${country.Country}</td>
+        <td>${country.TotalConfirmed}</td>
+        <td>${country.TotalDeaths}</td>
+        <td>${(country.TotalDeaths/country.TotalConfirmed)}%</td>
+        <td>${country.NewDeaths}</td>
+      `;
+      document.getElementById('cases-by-country').appendChild(row);
+    });
+  });
+
