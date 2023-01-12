661bcec628b8b1e7f2bff3b498ab1fe01e09b09f
Jared <68973848+JRH89@users.noreply.github.com>
2023-01-12 08:00:03 -0800

    B

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ fetch(apiUrl)
     const errorCard = document.createElement('div');
     errorCard.classList.add('error-card');
     errorCard.innerHTML = `
-      <p>An error occurred while fetching COVID-19 data:</p>
-      <p>Please try again in a few moments.</p>
+      <p>Data is being updated or an error occured: Please try again in a few moments.</p>
     `;
     document.body.appendChild(errorCard);
   });
